2532dfa94c8d14b907472795391e9a909de0f14d
Gerahara <gerfersie@gmail.com>
2022-07-04 10:01:02 +0200

    Deploy Heroku 2

--- src/features/products/productsService.js
+++ src/features/products/productsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://ecommerce-clase-backend.herokuapp.com";
 
 const getAll = async () => {
   const res = await axios.get(API_URL + "/products");
